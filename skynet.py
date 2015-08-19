ed5c24929b65af68dd8b2dfb513db8fc76af801d
Paddy Polson <paddypolson@gmail.com>
2015-08-19 15:23:37 +1200

    AI has better target acquisition
    The AI will cycle through the balls coming towards it and then the balls
    heading away. Picking the highest threat.

--- skynet.py
+++ skynet.py
@@ -12,17 +12,47 @@ class AiPlayer(pong.Player):
 
     def target_ball(self, balls):
 
-        closest = balls[0]
+        # Find a new target if the old target has died
+        if not (self.target in balls):
+            self.target = balls[0]
+
+        # Grab all the possible target candidates
+        candidates = []
+        # And remember the balls heading in the other direction
+        secondary = []
 
         for ball in balls:
 
             if ball.direction.d[0] > 0:
+                candidates.append(ball)
+
+            else:
+                secondary.append(ball)
+
+        # Check for any possible target
+        if candidates:
+            # Find the closest target
+            closest = candidates[0]
+
+            for ball in candidates:
 
                 if ball.position[0] > closest.position[0]:
 
                     closest = ball
 
-        self.target = closest
+            self.target = closest
+
+        else:
+            # Look for balls heading in the other direction
+            closest = secondary[0]
+
+            for ball in secondary:
+
+                if ball.position[0] < closest.position[0]:
+
+                    closest = ball
+
+            self.target = closest
 
     def update(self):
 
